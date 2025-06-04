7f1214ca9d91a94ada8440168b68f7f876d1aeb9
harry123h <sciencebytes7055@gmail.com>
2025-06-05 00:04:20 +0530

    Update JS code

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from the repq folder
+// Serve all static files (CSS, JS, images) from "repq"
 app.use(express.static(path.join(__dirname, 'repq')));
 
-// Serve index.html on root
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'hello', 'index.html'));
+// Serve HTML files directly by name
+app.get('/:page', (req, res) => {
+  const page = req.params.page;
+  res.sendFile(path.join(__dirname, 'repq', page));
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// Default route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'repq', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
